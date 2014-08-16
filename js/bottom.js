69ce482f5cc19e6eccd0c0c3adc222bba2d97a72
JafeChang <jafechang@gmail.com>
2014-08-16 17:01:53 +0800

    test

--- js/bottom.js
+++ js/bottom.js
@@ -1,2 +1,2 @@
-parent.document.all("ct2ifm").height=document.body.height; 
-parent.document.all("ct2ifm").width =document.body.width ; 
+parent.document.all("ct1ifm").style.height=document.body.scrollHeight; 
+parent.document.all("ct1ifm").style.width=document.body.scrollWidth; 
