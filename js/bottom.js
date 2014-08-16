141d617f8e8e32d34bb4d0d50777f85817e7e9a7
JafeChang <jafechang@gmail.com>
2014-08-16 18:03:26 +0800

    test

--- js/bottom.js
+++ js/bottom.js
@@ -1,4 +1,7 @@
 // var top = document.documentElement.scrollTop ? 
 // document.documentElement.scrollTop : document.body.scrollTop;
 //alert(document.documentElement.scrollHeight);
-parent.document.all("ct1ifm").style.height=document.documentElement.scrollHeight; 
+function load(){
+	//alert(document.documentElement.scrollHeight);
+	parent.document.all("ct1ifm").style.height=document.documentElement.scrollHeight; 
+}
