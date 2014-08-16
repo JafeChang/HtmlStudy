01c9ca57112b9097f3842776460642db52ae765a
JafeChang <jafechang@gmail.com>
2014-08-16 17:31:20 +0800

    test

--- js/bottom.js
+++ js/bottom.js
@@ -1,2 +1,5 @@
-parent.document.all("ct1ifm").style.height=document.body.scrollHeight; 
-parent.document.all("ct1ifm").style.width=document.body.scrollWidth; 
+// var top = document.documentElement.scrollTop ? 
+// document.documentElement.scrollTop : document.body.scrollTop;
+// alert(document.documentElement.scrollHeight);
+parent.document.all("ct1ifm").style.height=document.documentElement.scrollHeight; 
+parent.document.all("ct1ifm").style.width=document.documentElement.scrollWidth; 
